38d6fdc04c650bc9621c1acd296c2bc1b871ecac
BolotinYa <69606505+BolotinYa@users.noreply.github.com>
2020-10-03 17:43:17 +0300

    Add files via upload

---
+++ js/searchTS.js
@@ -0,0 +1,15 @@
+var input = document.getElementById('input');
+input.addEventListener('input', changeList);
+function changeList() {
+    // делаем из нашего списка объект
+    var carList = document.getElementsByClassName('rep-name');
+    // сам поиск
+    for (var i = 0; i < carList.length; i++) {
+        if (carList[i].innerText.indexOf(input.value) < 0) {
+            carList[i].style.display = 'none';
+        }
+        else {
+            carList[i].style.display = 'block';
+        }
+    }
+}
